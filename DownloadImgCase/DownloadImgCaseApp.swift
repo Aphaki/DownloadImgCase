d79694d6ed83ac5d3b434b6c8b635418c322a327
Aphaki <kiapha1108@gmail.com>
2022-06-20 10:55:25 +0900

    Initial Commit

---
+++ DownloadImgCase/DownloadImgCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadImgCaseApp.swift
+//  DownloadImgCase
+//
+//  Created by Sy Lee on 2022/06/20.
+//
+
+import SwiftUI
+
+@main
+struct DownloadImgCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
